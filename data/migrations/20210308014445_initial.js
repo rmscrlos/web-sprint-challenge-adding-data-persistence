5bbd81e2a40ace94ba9ac7c8f04efde85f387d72
Carlos Ramos <rmscrlos@gmail.com>
2021-03-07 20:57:50 -0500

    created tables in migration file

---
+++ data/migrations/20210308014445_initial.js
@@ -0,0 +1,28 @@
+exports.up = async function (knex) {
+	await knex.schema.createTable('projects', table => {
+		table.increments('project_id');
+		table.text('project_name').notNull();
+		table.text('project_description');
+		table.boolean('project_completed').defaultTo(false);
+	});
+
+	await knex.schema.createTable('resource', table => {
+		table.increments('resource_id');
+		table.text('resource_name').notNull().unique();
+		table.text('resource_description');
+	});
+
+	await knex.schema.createTable('task', table => {
+		table.increments('task_id');
+		table.text('task_description').notNull();
+		table.text('task_notes');
+		table.boolean('task_completed').defaultTo(false);
+		table.integer('project_id').notNull().references('project_id').inTable('projects');
+	});
+};
+
+exports.down = async function (knex) {
+	await knex.schema.dropTableIfExists('task');
+	await knex.schema.dropTableIfExists('resource');
+	await knex.schema.dropTableIfExists('projects');
+};
