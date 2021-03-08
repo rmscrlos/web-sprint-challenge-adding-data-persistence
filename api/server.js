e3155ae5149e001427fe3556d5a422fcd7b32372
Carlos Ramos <rmscrlos@gmail.com>
2021-03-07 20:43:44 -0500

    installed denpendencies and completed tasks ibuild index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).json({
+		message: 'Something went wrong. Please, try again later.'
+	});
+});
